fe173ab01f8779891a378489c9406a7ed7a3085d
getdn <getdna99@gmail.com>
2023-03-23 01:07:56 -0700

    Add AttachedPropertyGenerator

--- EasyXAMLTools.Generator/AttachedPropertyAttribute.cs
+++ EasyXAMLTools.Generator/AttachedPropertyAttribute.cs
@@ -4,11 +4,42 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace EasyXAMLTools;
-[AttributeUsage(AttributeTargets.Class)]
-class AttachedPropertyAttribute<T> : Attribute
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+class AttachedPropertyAttribute : Attribute
 {
-    public AttachedPropertyAttribute(string Name)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="PropertyType">Property Type</param>
+    /// <param name="Name">Property Name</param>
+    /// <param name="DependencyObjectType">Dependency Object Type that PropertyType can be set for. If null, uses DependencyObject</param>
+    public AttachedPropertyAttribute(Type PropertyType, string Name, Type? DependencyObjectType = null)
     {
 
     }
+    /// <summary>
+    /// Whether to use Nullable Reference Type annotation or not
+    /// </summary>
+    public bool UseNullableReferenceType { get; set; } = false;
+    /// <summary>
+    /// Whether Local OnPropertyChanged Method should be generated
+    /// </summary>
+    public bool GenerateLocalOnPropertyChangedMethod { get; set; } = false;
+    /// <summary>
+    /// Whether Local OnPropertyChanged Method should be generated with property value paramter
+    /// </summary>
+    public bool LocalOnPropertyChangedMethodWithPropertyValueParameter { get; set; } = true;
+    /// <summary>
+    /// Whether Local OnPropertyChanged Method should be generated with partial modifier
+    /// If null, It is equal to LocalOnPropertyChangedMethodWithParameter
+    /// </summary>
+    public bool? LocalOnPropertyChangedMethodWithPartial { get; set; } = null;
+    /// <summary>
+    /// Name of Local OnPropertyChanged method (null for default name)
+    /// </summary>
+    public string? LocalOnPropertyChangedMethodName { get; set; } = null;
+    /// <summary>
+    /// Documentation As String to put on the property
+    /// </summary>
+    public string? Documentation { get; set; } = null;
 }
